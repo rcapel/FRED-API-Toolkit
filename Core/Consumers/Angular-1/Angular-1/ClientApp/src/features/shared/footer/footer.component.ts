c5a22fff1a3af169abd2efd63486b775af514b56
rod capel <rcapel@comcast.net>
2018-08-16 11:29:21 -0400

    initial commit

---
+++ Core/Consumers/Angular-1/Angular-1/ClientApp/src/features/shared/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'footer',
+  templateUrl: './footer.component.html'
+})
+export class FooterComponent {
+
+  @Input()
+  fetchMessage: string;
+  @Input()
+  url: string;
+
+}
