16292a7a2f0dfbb363033d539ee1fc2bcb324939
rod capel <rcapel@comcast.net>
2018-08-17 08:29:02 -0400

    added group_popularity to series-related order by

--- Core/FREDApi/FREDApi/Core/FREDData.cs
+++ Core/FREDApi/FREDApi/Core/FREDData.cs
@@ -12,8 +12,8 @@
 		public enum filter_value_values { all, macro, regional }
 		public enum tag_group_id_values { freq, gen, geo, geot, rls, seas, src }
 
-		public enum series_order_by_values { series_id, title, units, frequency, seasonal_adjustment, realtime_start, realtime_end, last_updated, observation_start, observation_end, popularity }
-		public enum series_search_order_by_values { search_rank, series_id, title, units, frequency, seasonal_adjustment, realtime_start, realtime_end, last_updated, observation_start, observation_end, popularity }
+		public enum series_order_by_values { series_id, title, units, frequency, seasonal_adjustment, realtime_start, realtime_end, last_updated, observation_start, observation_end, popularity, group_popularity }
+		public enum series_search_order_by_values { search_rank, series_id, title, units, frequency, seasonal_adjustment, realtime_start, realtime_end, last_updated, observation_start, observation_end, popularity, group_popularity }
 		public enum sources_order_by_values { source_id, name, realtime_start, realtime_end }
 		public enum tags_order_by_values { series_count, popularity, created, name, group_id }
 		public enum releases_order_by_values { release_id, name, press_release, realtime_start, realtime_end }
