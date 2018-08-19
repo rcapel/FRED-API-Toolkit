a34a20ad30205969ae82a167f6abfc31da527c70
rod capel <rcapel@comcast.net>
2018-08-19 18:07:56 -0400

    initial commit

---
+++ Core/Consumers/Angular-1/Angular-1/ClientApp/src/features/shared/sourcesGrid/sourcesGridHeader.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'sourcesGridHeader',
+  templateUrl: './sourcesGridHeader.component.html'
+})
+export class SourcesGridHeaderComponent {
+}
---
+++ Core/Consumers/Angular-1/Angular-1/ClientApp/src/features/shared/tagsGrid/tagsGridDetail.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+import { ITag } from '../../../fredapi/tags/tag.interfaces';
+
+@Component({
+  selector: 'tagsGridDetail',
+  templateUrl: './tagsGridDetail.component.html'
+})
+export class TagsGridDetailComponent {
+  @Input() tag: ITag;
+
+}
