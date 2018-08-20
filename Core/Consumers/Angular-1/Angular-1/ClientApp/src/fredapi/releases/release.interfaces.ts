0842f0bf945c357fd93be53ee035f0557f6a0f41
rod capel <rcapel@comcast.net>
2018-08-20 11:42:43 -0400

    use extensions to shared interfaces

--- Core/Consumers/Angular-1/Angular-1/ClientApp/src/fredapi/releases/release.interfaces.ts
+++ Core/Consumers/Angular-1/Angular-1/ClientApp/src/fredapi/releases/release.interfaces.ts
@@ -1,25 +1,15 @@
 import { ISource } from "../sources/source.interfaces";
+import { IContainerExtensions, IFilters } from "../shared/shared.interfaces";
 
-export interface IReleaseResponse {
+export interface IReleaseResponse extends IContainerExtensions {
   container: IReleaseContainer;
-  fetchMessage: string;
-  url: string;
 }
 
-export interface IReleaseContainer {//todo:extend filters interface
-  realtime_start: Date;
-  realtime_end: Date;
-  count: number;
-  limit: number;
-  offset: number;
-  order_by: number;
-  sort_order: number;
-
+export interface IReleaseContainer extends IFilters {
   releases: IRelease[];
 }
 
 export interface IRelease extends ISource {
-  //sourceItem: ISource;
   press_release: boolean;
 }
 
--- Core/Consumers/Angular-1/Angular-1/ClientApp/src/fredapi/shared/shared.interfaces.ts
+++ Core/Consumers/Angular-1/Angular-1/ClientApp/src/fredapi/shared/shared.interfaces.ts
@@ -1,18 +1,14 @@
-//export interface IApi {
-//  fetchMessage: string;
-//  url: string;
-//}
+export interface IContainerExtensions {
+  fetchMessage: string;
+  url: string;
+}
 
-//export interface IFilters {
-//  realtime_start: Date;
-//  realtime_end: Date;
-//}
-
-//export interface IExtendedFilters {
-//  container: IFilters;
-//  order_by: number;
-//  sort_order: number;
-//  count: number;
-//  offset: number;
-//  limit: number;
-//}
+export interface IFilters {
+  realtime_start: Date;
+  realtime_end: Date;
+  order_by: number;
+  sort_order: number;
+  count: number;
+  offset: number;
+  limit: number;
+}
