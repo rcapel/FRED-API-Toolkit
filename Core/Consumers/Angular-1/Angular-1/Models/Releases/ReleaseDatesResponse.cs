6e3c6d2aec211b44191bd2e4c08b05958dc9efcc
rod capel <rcapel@comcast.net>
2018-08-20 10:41:32 -0400

    initial commit

---
+++ Core/Consumers/Angular-1/Angular-1/Models/Releases/ReleaseDatesResponse.cs
@@ -0,0 +1,14 @@
+﻿using FRED.Api.Releases.Data;
+
+namespace AngularConsumer1.Models.Releases
+{
+	public class ReleaseDatesResponse : ApiData
+	{
+		#region properties
+
+		public ReleaseDateContainer container { get; set; }
+
+		#endregion
+
+	}
+}
