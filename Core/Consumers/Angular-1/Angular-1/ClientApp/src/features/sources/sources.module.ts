5c222e4c52b2609c5de6d7083e9d1e6b22ab45b6
rod capel <rcapel@comcast.net>
2018-08-21 10:02:13 -0400

    added routes, components and resolvers for source and sourceReleases

--- Core/Consumers/Angular-1/Angular-1/ClientApp/src/features/sources/sources.module.ts
+++ Core/Consumers/Angular-1/Angular-1/ClientApp/src/features/sources/sources.module.ts
@@ -6,22 +6,33 @@ import { RouterModule } from '@angular/router';
 import { FeaturesCommonModule } from '../featuresCommon.module';
 
 import { SourcesComponent } from './sources/sources.component';
+import { SourceComponent } from './source/source.component';
+import { SourceReleasesComponent } from './sourceReleases/sourceReleases.component';
+
 import { SourcesResolver } from '../sources/services/sources.resolver';
+import { SourceResolver } from '../sources/services/source.resolver';
+import { SourceReleasesResolver } from '../sources/services/sourceReleases.resolver';
 
 @NgModule({
   declarations: [
-    SourcesComponent
+    SourcesComponent,
+    SourceComponent,
+    SourceReleasesComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forChild([
-      { path: 'sources', component: SourcesComponent, resolve: { sources: SourcesResolver } }
+      { path: 'sources', component: SourcesComponent, resolve: { sources: SourcesResolver } },
+      { path: 'source/:id', component: SourceComponent, resolve: { source: SourceResolver } },
+      { path: 'sourceReleases/:id', component: SourceReleasesComponent, resolve: { sourceReleases: SourceReleasesResolver } }
     ]),
     FeaturesCommonModule
   ],
   providers: [
-    SourcesResolver
+    SourcesResolver,
+    SourceResolver,
+    SourceReleasesResolver
   ]
 })
 export class SourcesModule { }
