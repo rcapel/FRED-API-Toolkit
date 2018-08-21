1dcb386f09156eeeae863d68b607d2803eecb359
rod capel <rcapel@comcast.net>
2018-08-21 10:33:58 -0400

    added service methods

--- Core/Consumers/Angular-1/Angular-1/ClientApp/src/fredapi/sources/source.service.ts
+++ Core/Consumers/Angular-1/Angular-1/ClientApp/src/fredapi/sources/source.service.ts
@@ -3,15 +3,30 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { ISourceResponse } from './source.interfaces'
+import { IReleaseResponse } from '../releases/release.interfaces';
 
 @Injectable()
 export class SourceService {
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+  constructor(private client: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
   }
 
   public get(): Observable<ISourceResponse> {
-    return this.http.get<ISourceResponse>(this.baseUrl + 'api/sources');
+    return this.client.get<ISourceResponse>(this.baseUrl + 'api/sources');
+  }
+
+  public getSource(sourceId: number): Observable<ISourceResponse> {
+    let apiRoute = this.baseUrl + 'api/source/' + sourceId;
+    console.log(apiRoute);
+
+    return this.client.get<ISourceResponse>(apiRoute);
+  }
+
+  public getSourceReleases(sourceId: number): Observable<IReleaseResponse> {
+    let apiRoute = this.baseUrl + 'api/sourceReleases/' + sourceId;
+    console.log(apiRoute);
+
+    return this.client.get<IReleaseResponse>(apiRoute);
   }
 
 }
