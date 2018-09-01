0057208a5fad247146604df406704b2a68744aff
rod capel <rcapel@comcast.net>
2018-09-01 15:42:10 -0400

    add back ignored files

---
+++ Version 2.0/Consumers/Angular-1/Angular-1/Models/Releases/ReleaseResponse.cs
@@ -0,0 +1,14 @@
+﻿using FRED.Api.Releases.Data;
+
+namespace AngularConsumer1.Models.Releases
+{
+	public class ReleaseResponse : ApiData
+	{
+		#region properties
+
+		public ReleaseContainer container { get; set; }
+
+		#endregion
+
+	}
+}
---
+++ Version 2.0/Consumers/Angular-1/Angular-1/Models/Releases/ReleasesDatesResponse.cs
@@ -0,0 +1,14 @@
+﻿using FRED.Api.Releases.Data;
+
+namespace AngularConsumer1.Models.Releases
+{
+	public class ReleasesDatesResponse : ApiData
+	{
+		#region properties
+
+		public ReleasesDateContainer container { get; set; }
+
+		#endregion
+
+	}
+}
