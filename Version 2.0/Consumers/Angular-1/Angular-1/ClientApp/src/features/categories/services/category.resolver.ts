c6f742117428db04f270256c02ffaacff17552a5
rod capel <rcapel@comcast.net>
2018-09-15 16:02:58 -0400

    removed comments

--- Version 2.0/Consumers/Angular-1/Angular-1/ClientApp/src/features/categories/services/category.resolver.ts
+++ Version 2.0/Consumers/Angular-1/Angular-1/ClientApp/src/features/categories/services/category.resolver.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable, /*throwError*/ } from 'rxjs';
-//import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { CategoryService } from '../../../fredapi/categories/category.service';
 import { ICategoryResponse } from '../../../fredapi/categories/category.interfaces';
@@ -17,19 +15,7 @@ export class CategoryResolver implements Resolve<ICategoryResponse>{
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ICategoryResponse> {
     let categoryId: string = route.paramMap.get('id');
-    //alert('from resolver: category ' + categoryId);
-
-    //if (isNaN(categoryId)) {
-    //  console.log("${categoryId} is not a number.");
-    //  this.router.navigate(["/"]);
-    //  return Observable.of(null);
-    //}
     return this.service.get(+categoryId);
-      //.pipe(catchError(this.handleError));
   }
-
-  //private handleError(error: HttpErrorResponse): Observable<never> {
-  //  return throwError(error);
-  //}
 
 }
